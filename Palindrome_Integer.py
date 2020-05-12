52556e4611f65e4874fa093b72e2f8e3583cc473
bioright <64993249+bioright@users.noreply.github.com>
2020-05-12 19:56:00 -0400

    Experiencing Commit issues

---
+++ Palindrome_Integer.py
@@ -0,0 +1,17 @@
+# palindrome integer
+# Determine whether an integer is a palindrome. 
+# An integer is a palindrome when it reads the same backward as forward.
+# Note that this approach is not the best when aiming to save on memory space. but it is the easiest.
+
+def is_palindrome(x):
+                         # first we establish that a negative integer cannot be a palindrome by definition
+    if x<0:                          # This logic can be skipped because it is implied in step 2.
+        print("No")
+                                     # 2nd logic is to convert the int to str in order to slice it
+    elif str(x)[::-1] == str(x)[0:]: # compares the reversed str with the original str
+        print("Yes")
+    else:                            # for all other cases
+        print("No")
+is_palindrome(0)                   # put any integer in the function
+
+
